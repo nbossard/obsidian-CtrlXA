cd9130d620d921b168bcd2ba098817ad3a208184
Nicolas Bossard <nicolas.bossard@orange.com>
2023-12-15 17:14:40 +0100

    implementing cycle up

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView,  Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { findCycle } from './cycle';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,57 +11,31 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class CtrlXAPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'cycle-up',
+			name: 'Cycle up',
+			editorCallback: (editor: Editor, _view: MarkdownView) => {
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				console.log("Line >" + editor.getLine(editor.getCursor().line) + "<");
+				console.log("Word from >" + (editor.wordAt(editor.getCursor())?.from.ch ?? "") + "<");
+				console.log("Word to >" + (editor.wordAt(editor.getCursor())?.to.ch ?? "") + "<");
+
+				let wordAt = editor.wordAt(editor.getCursor());
+				if (wordAt != null) {
+					let wordToReplace = editor.getRange(wordAt.from, wordAt.to);
+					console.log("Replacing word >" + wordToReplace + "<");
+					let wordNew = findCycle(wordToReplace,1);
+					console.log("New word >" + wordNew + "<");
+					editor.replaceRange(wordNew, wordAt.from, wordAt.to);
+				} else {
+					console.log("No word at cursor, doing nothing");
 				}
 			}
 		});
