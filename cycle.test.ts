0691ae46221e9b058530e1e406076b0a235c2461
Nicolas Bossard <nicolas.bossard@orange.com>
2023-12-15 17:16:40 +0100

    Adding cycle.ts

---
+++ cycle.test.ts
@@ -0,0 +1,21 @@
+import { findCycle } from './cycle'
+
+describe('findCycle function', () => {
+  it('should return the next word in the cycle when going up', () => {
+    expect(findCycle('Lundi', 1)).toBe('Mardi');
+    expect(findCycle('vendredi', 1)).toBe('Samedi');
+    expect(findCycle('Sunday', 1)).toBe('Monday');
+  });
+
+  it('should return the previous word in the cycle when going down', () => {
+    expect(findCycle('Mardi', -1)).toBe('Lundi');
+    expect(findCycle('Samedi', -1)).toBe('Vendredi');
+    expect(findCycle('Wednesday', -1)).toBe('Tuesday');
+  });
+
+  it('should return the same word if parCurWord is not found in any cycle', () => {
+    expect(findCycle('Nonexistent', 1)).toBe('Nonexistent');
+    expect(findCycle('Nonexistent', -1)).toBe('Nonexistent');
+  });
+});
+
---
+++ cycle.ts
@@ -0,0 +1,37 @@
+
+/** function findCycle
+ *
+* will find next word in cycle of words
+* will return same word if parCurWord is not found in any cycle
+*
+* @example findCycle("Lundi", 1) will return "Mardi"
+*
+* @param parCurWord: current word to be replaced
+* @param parDirection: 1 for going up, -1 for going down
+* @return next word in cycle
+*/
+export function findCycle(parCurWord: string, parDirection: number): string {
+	let cycles: string[][] = [
+		["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+		["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
+		["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	];
+	let curCycle: string[];
+	let curCycleIndex: number;
+	let curCycleWordIndex: number;
+
+	for (let i = 0; i < cycles.length; i++) {
+		curCycle = cycles[i];
+		curCycleIndex = curCycle.indexOf(parCurWord);
+		if (curCycleIndex >= 0) {
+			curCycleWordIndex = curCycleIndex + parDirection;
+			if (curCycleWordIndex < 0) {
+				curCycleWordIndex = curCycle.length - 1;
+			} else if (curCycleWordIndex >= curCycle.length) {
+				curCycleWordIndex = 0;
+			}
+			return curCycle[curCycleWordIndex];
+		}
+	}
+	return parCurWord;
+}
